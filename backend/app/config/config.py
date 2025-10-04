e8f7aef1d35ccffe64c0cdbe9f1f899cfcf419f7
rishabh11336 <rishabh11336@gmail.com>
2025-10-04 21:28:33 +0530

    commented config for redis : config.py

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -9,7 +9,7 @@ class Config:
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.urandom(24)
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
-    CACHE_TYPE= 'RedisCache'
-    CACHE_REDIS_URL= 'redis://localhost:6379/3'
-    CACHE_DEFAULT_TIMEOUT= 10
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=0.25)  # 15 minutes
+    # CACHE_TYPE= 'RedisCache'
+    # CACHE_REDIS_URL= 'redis://localhost:6379/3'
+    # CACHE_DEFAULT_TIMEOUT= 10
