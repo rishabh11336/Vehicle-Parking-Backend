8f00c5f32b703b9a9af03c689dffa4210e3b70f7
rishabh11336 <rishabh11336@gmail.com>
2025-10-04 23:43:59 +0530

    app.run changes to make it open on local port

--- backend/main.py
+++ backend/main.py
@@ -17,4 +17,4 @@ def setup_periodic_tasks(sender, **kwargs):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
