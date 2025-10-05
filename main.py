d175921e7719b5a421e1ae6e749be803de5d0f74
rishabh11336 <rishabh11336@gmail.com>
2025-10-05 17:24:19 +0530

    testing 5

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ import os
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), debug=True)
